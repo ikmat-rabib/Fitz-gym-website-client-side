73334749693258d2e99eeceb55ff1e62c244d772
ikmat-rabib <ikmat.rbb@gmail.com>
2023-11-25 01:42:43 +0600

    created homepage,navbar,footer,errorpage

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,16 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Components/Footer/Footer";
+import Navbar from "../Components/Navbar/Navbar";
 
 
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
+            <div className="max-w-screen-xl mx-auto">
             <Outlet></Outlet>
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
