b9bce1580a305e06cdab3e09b53f2bfa206a85eb
ikmat-rabib <ikmat.rbb@gmail.com>
2023-11-25 00:42:58 +0600

    first commit

---
+++ src/Pages/HomePage/Home/HomePage.jsx
@@ -0,0 +1,11 @@
+
+
+const HomePage = () => {
+    return (
+        <div>
+           <h2> homeeeeeee</h2>
+        </div>
+    );
+};
+
+export default HomePage;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,18 @@
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+import Main from "../Layout/Main";
+import HomePage from "../Pages/HomePage/Home/HomePage";
+
+  export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+            path: '/',
+            element: <HomePage></HomePage>
+        }
+      ]
+    },
+  ]);
